b231aec6403fccf9172b771b5931278fc403aaa6
Vetri2811 <vetriraja28112002@gmail.com>
2025-07-24 11:13:39 +0530

    adding escape sequences basic codes

---
+++ print_functions/escape_sequence2.py
@@ -0,0 +1,8 @@
+print('\'it\'s Hero time!\'',end='\n\t\t\t\t-Ben 10')
+
+print("\n\n'it's Hero time!'",end='\n\t\t\t\t-Ben 10')
+
+'''
+\n is used for new line
+\t is used for tab spaces
+'''
---
+++ print_functions/escape_sequences.py
@@ -0,0 +1,16 @@
+#print('vetri's phone')
+"""
+in above string there are three (') by this python was confused
+because we need output of vetri's phone so we put 'vetri's phone'
+by this python was confused and we can also solve this issue by using escape sequences()\\
+by using escape sequences we can convert normal character into special meaningfull character
+and also convert  special meaningfull character into normal character
+"""
+print('vetri\'s phone')
+
+#print("he said, "this is my phone")
+#desired outputs he said, "this is my phone"
+print("he said, \"this is my phone\"",end='\n\n\n\n\n')
+
+print("this is backslash: \\")
+
